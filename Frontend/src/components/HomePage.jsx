4db53690f41b940ea77a2e0a8d3de09a23985acc
Nick Schafer <133047743+Nmsviper93@users.noreply.github.com>
2024-07-24 20:24:22 -0400

    Add files via upload

---
+++ Frontend/src/components/HomePage.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import recipeService from '../services/recipeService';
+import 'HomePage.css';
+
+const HomePage = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    // useEffect hook to fetch recipes when component mounts
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                // call service to fetch all recipes
+                const data = await recipeService.getAllRecipes();
+                // update with fetched recipes
+                setRecipes(data);
+                setLoading(false);
+            } catch (err) {
+                setError('Failed to fetch recipes');
+                setLoading(false);
+            }
+        };
+
+        // fetch recipes when component mounts
+        fetchRecipes();
+        // empty dependency array to ensure only one run on mount
+    }, []);
+
+    // conditional rendering based on loading and error state
+    if (loading) return<p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+
+    return (
+        <div className="home-container">
+            <h2>All Vegan Recipes</h2>
+            <ul>
+                {recipes.map((recipe) => (
+                    <li key={recipe._id}>{recipe.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default HomePage;
