0cad8f3c2b9a2ce92f46bbb57d27196db446afe4
Jesse Brink <jbrink0925@gmail.com>
2024-07-27 01:41:04 -0700

    More changes.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage';
+import SearchPage from './components/SearchPage';
+import ResultsPage from './components/ResultsPage';
+import FavoritesPage from './components/FavoritesPage';
+import ProfilePage from './components/ProfilePage';
+import ProfileEditPage from './components/ProfileEditPage';
+import RegisterPage from './components/RegisterPage';
+import RecipeDetailsPage from './components/RecipeDetailsPage';
+import Recipes from './components/Recipes';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className="app-container">
+        <Router>
+            <header>
+                    <h1>Vegan Recipes</h1>
+                    <nav>
+                        <a href="/">Home</a>
+                        <a href="/search">Search</a>
+                        <a href="/favorites">Favorites</a>
+                        <a href="/profile">Profile</a>
+                        <a href="/login">Login</a>
+                    </nav>
+                </header>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/search" component={SearchPage} />
+                    <Route path="/results" component={ResultsPage} />
+                    <Route path="/favorites" component={FavoritesPage} />
+                    <Route path="/profile" exact component={ProfilePage} />
+                    <Route path="/profile/edit" component={ProfileEditPage} />
+                    <Route path="/login" component={LoginPage} />
+                    <Route path="/register" component={RegisterPage} />
+                    <Route path="/recipe/:id" component={RecipeDetailsPage} />
+                    <Route path="/recipes" component={Recipes} />
+                </Switch>
+                <footer>
+                    <p>&copy; 2024 Vegan Recipes</p>
+                </footer>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
