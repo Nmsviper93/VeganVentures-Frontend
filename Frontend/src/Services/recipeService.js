2f3141c1c54290c97daf56d3dee14dbb3bbbd9b1
Nick Schafer <133047743+Nmsviper93@users.noreply.github.com>
2024-07-30 16:07:23 -0400

    Update recipeService.js

--- Frontend/src/Services/recipeService.js
+++ Frontend/src/Services/recipeService.js
@@ -1,31 +1,39 @@
 import axios from 'axios';
 
-const API_URL = 'https://spoonacular.com/food-api';
+const API_URL = 'https://api.spoonacular.com/recipes';
+const API_KEY = 'd74a060a9a3b426f9689dc4e20d2f1ce';
 
-// function to fetch all recipes by ID
-const getAllRecipes = async () => {
+// function to fetch all vegan recipes
+const getAllVeganRecipes = async () => {
     try {
-        // GET request to fetch all recipes
-        const response = await axios.get(`${API_URL}/recipes`);
-        return response.data;
+        // GET request to fetch vegan recipes
+        const response = await axios.get(`${API_URL}/complexSearch`, {
+            params: {
+                diet: 'vegan',
+                number: 20,
+                apiKey: d74a060a9a3b426f9689dc4e20d2f1ce,
+                sort: 'random'
+            }
+        });
+        return response.data.results;
     }  catch (err) {
         // log errors, throw error to be handled by the calling function
-        console.error('Error fetching recipe details', err);
+        console.error('Error fetching vegan recipes', err);
         throw err;
     }
 };
 
 // function to fetch recipe by ID
-const getRecipeById = async (id) => {
-    try {
-        // GET request to fetch recipe details by ID
-        const response = await axios.get(`${API_URL}/recipes/${id}`); 
-            return response.data;
-    } catch (err) {
-        console.error('Error searching recipes', err);
-        throw err;
-    }
-};
+// const getRecipeById = async (id) => {
+//     try {
+//         // GET request to fetch recipe details by ID
+//         const response = await axios.get(`${API_URL}/recipes/${id}`); 
+//             return response.data;
+//     } catch (err) {
+//         console.error('Error searching recipes', err);
+//         throw err;
+//     }
+// };
 
 export default {
     getAllRecipes,
