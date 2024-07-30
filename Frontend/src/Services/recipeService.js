1909a051283ce3555ea6b2674a7d1ec6866d6aa8
Nick Schafer <133047743+Nmsviper93@users.noreply.github.com>
2024-07-30 17:26:16 -0400

    Update recipeService.js

--- Frontend/src/Services/recipeService.js
+++ Frontend/src/Services/recipeService.js
@@ -3,6 +3,21 @@ import axios from 'axios';
 const API_URL = 'https://api.spoonacular.com/recipes';
 const API_KEY = 'd74a060a9a3b426f9689dc4e20d2f1ce';
 
+
+const getRecipeById = async (id) => {
+    try {
+        const response = await axios.get(`${API_URL}/${id}/information`, {
+            params: {
+                apiKey: 'd74a060a9a3b426f9689dc4e20d2f1ce',
+                }
+        });
+        return response.data;
+    } catch (err) {
+        console.error('Error fetching recipe details', err);
+        throw err;
+    }
+};
+
 // function to fetch all vegan recipes
 const getAllVeganRecipes = async () => {
     try {
@@ -26,4 +41,5 @@ const getAllVeganRecipes = async () => {
 
 export default {
     getAllVeganRecipes,
+    getRecipeById
 };
