e91d26f5eb041e085b9bfc1a0cbf3cb73c9b96a7
Nick Schafer <133047743+Nmsviper93@users.noreply.github.com>
2024-08-01 14:18:36 -0400

    Update authRoutes.js

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../Controllers/authController');
+const authController = require('../Controllers/authController');
 
 // create new router instance
 const router = express.Router();
@@ -8,11 +8,11 @@ const router = express.Router();
 // @route  POST request to 'api/auth/register'
 // desc    Register user
 // @access  Public
-router.post('/auth/register', authController.register);
+router.post('/register', authController.register);
 
 // @route  POST api/auth/login
 // @desc    Authenticate user & get token
 // @access  Public
-router.post('/auth/login', authController.login);
+router.post('/login', authController.login);
 
 module.exports = router;
