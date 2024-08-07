7c3c2ad2cc62c8a0ad9419e1452d335610acfb4e
Nick Schafer <133047743+Nmsviper93@users.noreply.github.com>
2024-08-07 15:47:59 -0400

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 import SearchPage from './components/SearchPage';
